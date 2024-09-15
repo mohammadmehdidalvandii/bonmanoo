975b444cb7e955fd95f69adf7517c6cb03a42c52
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-15 14:59:05 +0330

    add metaData fot page aboutUs

--- src/app/AboutUs/page.js
+++ src/app/AboutUs/page.js
@@ -4,6 +4,11 @@ import Navbar from '@/components/modules/Navbar/Navbar'
 import About from '@/components/template/about/About/About'
 import React from 'react'
 
+
+export const metadata = {
+    title:"بن مانو | درباره ما",
+}
+
 function page() {
   return (
     <>
