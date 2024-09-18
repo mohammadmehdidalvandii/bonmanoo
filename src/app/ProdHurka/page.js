3344c9d4da84d7f21417b8d3642ee3106169de88
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-18 16:54:01 +0330

    created page ProdHurka

---
+++ src/app/ProdHurka/page.js
@@ -0,0 +1,20 @@
+import Breadcrumb from '@/components/modules/Breadcrumb/Breadcrumb';
+import Footer from '@/components/modules/Footer/Footer';
+import Navbar from '@/components/modules/Navbar/Navbar';
+import PageHeader from '@/components/modules/PageHeader/PageHeader';
+import Shop from '@/components/template/product/Shop/Shop';
+import React from 'react';
+
+function page() {
+  return (
+    <>
+    <Navbar/>
+    <Breadcrumb name="محصولات هورکا"/>
+    <PageHeader title="محصولات هورکا"/>
+    <Shop/>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
