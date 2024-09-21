94bd0f59b3976dbaf1ae0d163a27357731c6c2c7
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-21 10:30:40 +0330

    created page productSingle and grid system

---
+++ src/app/Products/[id]/page.js
@@ -0,0 +1,33 @@
+import Breadcrumb from '@/components/modules/Breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/Footer/Footer'
+import Navbar from '@/components/modules/Navbar/Navbar'
+import React from 'react'
+
+function ProductSingle() {
+  return (
+   <>
+    <Navbar/>
+    <Breadcrumb name="جزئیات محصول"/>
+    <div className="containers">
+        <div className="row">
+            <div className="col-lg-9 col-md-9 col-sm-6 col-xs-12">
+                <div className="row">
+                    <div className="col-lg-6 col-md-12 col-sm-12">
+
+                    </div>
+                    <div className="col-lg-6 col-md-12 col-sm-12">
+
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                
+            </div>
+        </div>
+    </div>
+    <Footer/>
+   </>
+  )
+}
+
+export default ProductSingle
