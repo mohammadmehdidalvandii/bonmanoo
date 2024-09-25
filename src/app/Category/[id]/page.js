191afe1456c947b90a00101bdbee8eb828192180
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-09-25 11:24:00 +0330

    created page categoryDetails

---
+++ src/app/Category/[id]/page.js
@@ -0,0 +1,16 @@
+import Breadcrumb from '@/components/modules/Breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/Footer/Footer'
+import Navbar from '@/components/modules/Navbar/Navbar'
+import React from 'react'
+
+function CategoryDetails() {
+  return (
+    <>
+    <Navbar/>
+    <Breadcrumb name="جزئیات مقاله"/>
+    <Footer/>
+    </>
+  )
+}
+
+export default CategoryDetails
