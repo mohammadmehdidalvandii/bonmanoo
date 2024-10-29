155012976dfe8f6c469b3ce9a8e485a4ab98de70
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-10-29 15:27:16 +0330

    created page faq

---
+++ src/app/Faq/page.js
@@ -0,0 +1,18 @@
+import Breadcrumb from '@/components/modules/Breadcrumb/Breadcrumb'
+import Footer from '@/components/modules/Footer/Footer'
+import Navbar from '@/components/modules/Navbar/Navbar'
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <Navbar/>
+    <Breadcrumb name=" سوالات متداول"/>
+    <PageHeader title="سوالات متداول"/>
+    <Footer/>
+    </>
+  )
+}
+
+export default page
