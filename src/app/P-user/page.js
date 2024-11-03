ad66066c8d6d750b031b2fbf8ff42e54db043a8e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-03 14:07:20 +0330

    created page p-user

---
+++ src/app/P-user/page.js
@@ -0,0 +1,17 @@
+import UserLayout from '@/components/layout/UserLayout/UserLayout'
+import React from 'react'
+
+
+export const metadata = {
+    title:"بن مانو |  user-panel ",
+  }
+
+function page() {
+  return (
+    <UserLayout>
+        
+    </UserLayout>
+  )
+}
+
+export default page
