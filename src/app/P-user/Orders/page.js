dd2675e192536126700f4506f6cd7010501ab71b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-04 15:52:17 +0330

    created section orderList for order -purser page

--- src/app/P-user/Orders/page.js
+++ src/app/P-user/Orders/page.js
@@ -1,10 +1,11 @@
 import UserLayout from '@/components/layout/UserLayout/UserLayout'
+import OrderList from '@/components/template/p-user/OrderList/OrderList'
 import React from 'react'
 
 function page() {
   return (
     <UserLayout>
-        this is order
+        <OrderList/>
     </UserLayout>
   )
 }
