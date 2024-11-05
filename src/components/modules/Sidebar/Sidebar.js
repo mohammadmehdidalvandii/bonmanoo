8bb31b60dbcb6b7398ce54df115128f21d2d93de
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-05 14:39:36 +0330

    created page Credit-History

--- src/components/modules/Sidebar/Sidebar.js
+++ src/components/modules/Sidebar/Sidebar.js
@@ -52,7 +52,7 @@ function Sidebar() {
                 </Link>
               </li>
               <li className={style.sidebar_item}>
-                <Link href="/P-user" className={style.sidebar_item_link}>
+                <Link href="/P-user/Credit-History" className={style.sidebar_item_link}>
                   <span className={style.sidebar_item_icon}>
                     <IoSettingsOutline />
                   </span>
