7b3bb88672c63bdde6d677364ec794b06077cef6
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-05 15:36:10 +0330

    created section EditAccount for p-user

--- src/app/P-user/Edit-Account/page.js
+++ src/app/P-user/Edit-Account/page.js
@@ -1,10 +1,11 @@
 import UserLayout from '@/components/layout/UserLayout/UserLayout'
+import EditAccount from '@/components/template/p-user/EditAccount/EditAccount'
 import React from 'react'
 
 function page() {
   return (
     <UserLayout>
-        this is Edit Account
+        <EditAccount/>
     </UserLayout>
   )
 }
