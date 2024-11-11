aa2f434e9b37b254d18c9ff36105b166abe5ac68
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-11 15:08:59 +0330

    created model user

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const schema = new mongoose.Schema({
+    username:{
+        type : String,
+        required : true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required : true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        required : true,
+    },
+    refreshToken: String,
+
+});
+
+const model = mongoose.models.User || mongoose.model("User" , schema);
+
+export default model
