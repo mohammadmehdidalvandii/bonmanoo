8bbe1879c140fb0cf263432135dbb2fb46b1ef2b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-11 14:57:54 +0330

    created connections db for mongo

--- config/db.js
+++ config/db.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 const connectToDB = async ()=>{
-    try{} catch(error){
+    try{
+        if(mongoose.connections[0].readyState){
+            return true
+        } else{
+            await mongoose.connect(process.env.DATA_DB);
+            console.log("Connect TO DB is Successfully ✅")
+        }
+    } catch(error){
         console.log("DB Connections Has Error" , error)
     }
 }
