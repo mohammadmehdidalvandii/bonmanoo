5c5383aefdb3a12309a1b024037542ee1184b1a5
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-18 15:51:11 +0330

    created Apo Contact

---
+++ src/app/api/contact/route.js
@@ -0,0 +1,36 @@
+import connectToDB from "@/config/db";
+import ContactModel from '@/models/Contact';
+
+export async function POST (req){
+    try{
+        connectToDB();
+        
+        const body = await req.body();
+
+        const {
+            username,
+            phone,
+            email,
+            subject,
+            message, } = body
+
+            await ContactModel.create({
+                username,
+                phone,
+                email,
+                subject,
+                message,
+            });
+
+            return Response.json(
+                {message:"Contact Message is Successfully ✅"},
+                {status:201}
+            )
+
+    } catch(error){
+        return Response.json(
+            {message:"Error Server Contact =>" , error},
+            {status:500}
+        )
+    }
+}
