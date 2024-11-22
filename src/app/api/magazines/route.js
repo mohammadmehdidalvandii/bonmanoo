501a1bdd546d549627c81601b46904c590970513
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-11-22 09:47:13 +0330

    created api magazines

---
+++ src/app/api/magazines/route.js
@@ -0,0 +1,33 @@
+import connectToDB from "@/config/db";
+import MagazinesModel from '@/models/Magazines'
+
+export async function POST (req){
+    try {
+       await connectToDB();
+
+        const body  = await req.json();
+        const {name} = body;
+
+        if(!name){
+            return Response.json(
+                {message:"Name is Required "},
+                {status:400}
+            )  
+        }
+       const newMagazines =  await MagazinesModel.create({
+            name,
+        });
+
+        return Response.json(
+            {message:"Created Magazines Successfully", data:newMagazines},
+            {status:201}
+        )
+
+    } catch(error){
+        console.log("error=>", error)
+        return Response.json(
+            {message:"Error Server is Magazines =>", error},
+            {status:500}
+        )
+    }
+}
